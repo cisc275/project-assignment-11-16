9496ce20f76e39a286eb3710d50c7949cf215f75
suint <lui.ysia@gmail.com>
2019-05-17 16:51:32 -0400

    eating animation works

--- src/EatingHUD.java
+++ src/EatingHUD.java
@@ -2,6 +2,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
+import java.awt.image.RasterFormatException;
 
 /**
  * 
@@ -13,6 +14,7 @@ public class EatingHUD implements HUD, ImageObserver {
 	int frameWidth;
 	BufferedImage background;
 	BufferedImage[] timer;
+	int currentTime;
 	
 	public EatingHUD(int w, int h) {
 		frameWidth = w;
@@ -35,7 +37,13 @@ public class EatingHUD implements HUD, ImageObserver {
 			int numSprites = sheet.getWidth()/subsize;
 			imgs = new BufferedImage[numSprites];
 			for (int i = 0; i < numSprites; i++) {
-				
+				try {
+					imgs[i] = sheet.getSubimage(subsize*i, 0, subsize-1, subsize);
+				} catch (RasterFormatException e) {
+					imgs[i] = null;
+					System.out.println("loading frame " + (i+1) + " failed");
+					System.out.println(e);
+				}
 			}
 			return imgs;
 		} else {
@@ -45,11 +53,26 @@ public class EatingHUD implements HUD, ImageObserver {
 	}
 	
 	/**
-	 * args: score, scoregoal, time, maxtime
+	 * args: score, scoregoal, time elapsed, maxtime
+	 * @author - kelly
 	 */
 	public void paint(Graphics g, int[] args) {
-		//TODO do it better
 		g.drawString(Integer.toString(args[0]), 0, 0);
+		BufferedImage currentTimer= timer[getFrame(args[2], args[3])];
+		g.drawImage(currentTimer, frameWidth - currentTimer.getWidth(), frameHeight - currentTimer.getHeight(), this);
+		
+	}
+	
+	/**
+	 * pass in time elapsed and time limit to find current timer frame </br>
+	 * returns int
+	 * @author - kelly
+	 */
+	private int getFrame(int timeTaken, int maxTime) {
+		int a = timer.length;
+		float percent = (float) timeTaken/maxTime;
+		int frame = (int) (a * percent);
+		return frame; //int conversion truncates so that it won't go beyond array range
 	}
 	
 	/**
