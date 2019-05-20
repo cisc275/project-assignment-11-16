9a6c13b357c03468281bd605aaf61773aa4cf553
suint <lui.ysia@gmail.com>
2019-05-19 21:06:42 -0400

    merge artifact left in master oops

--- src/GameSequence.java
+++ src/GameSequence.java
@@ -16,20 +16,12 @@ public class GameSequence {
 	public GameSequence(int w, int h, boolean mig) {
 		elements = new LinkedList<Supplier<GameSequenceElement>>();
 		//elements.add(new GameSequenceElement(new Menu(w, h), new MenuHUD(w, h)));
-<<<<<<< HEAD
-		//elements.add(() -> new GameSequenceElement(new EatingTutorial(w, h), new EatingHUD(w, h)));
-		elements.add(() -> new GameSequenceElement(new EatingModel(w, h), new EatingHUD(w, h)));
-		//elements.add(() -> new GameSequenceElement(new MigratingTutorial(w, h, mig), null));
 
-		//elements.add(() -> new GameSequenceElement(new MigratingModel(w, h, mig), new MigratingHUD(w, h, mig)));
-		//elements.add(() -> new GameSequenceElement(new BreedingModel(w, h, mig), new BreedingHUD(w, h, mig)));
-=======
 		elements.add(() -> new GameSequenceElement(new EatingTutorial(w, h), new EatingHUD(w, h)));
 		elements.add(() -> new GameSequenceElement(new EatingModel(w, h, mig), new EatingHUD(w, h)));
 		elements.add(() -> new GameSequenceElement(new MigratingTutorial(w, h, mig), new MigratingHUD(w, h, mig)));
 		elements.add(() -> new GameSequenceElement(new MigratingModel(w, h, mig), new MigratingHUD(w, h, mig)));
 		elements.add(() -> new GameSequenceElement(new BreedingModel(w, h, mig), new BreedingHUD(w, h, mig)));
->>>>>>> master
 		itty = elements.iterator();
 	}
 	
